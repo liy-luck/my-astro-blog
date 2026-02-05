73b678aa6996bfeaedaea915f852a593bbde6f6a
liy <761686949@qq.com>
2026-02-05 23:28:58 +0800

    优化

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server', // 必须是 server，表示开启服务端渲染 (SSR)
   adapter: cloudflare(),
-  integrations: [tailwind()]
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
