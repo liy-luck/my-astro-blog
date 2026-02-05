4495566d0c4e5f3a68bcb082102a7c9c82ae92fd
liy <761686949@qq.com>
2026-02-05 22:50:10 +0800

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server', // 必须是 server，表示开启服务端渲染 (SSR)
+  adapter: cloudflare()
+});
---
+++ src/env.d.ts
@@ -0,0 +1,13 @@
+/// <reference path="../.astro/types.d.ts" />
+
+type D1Database = import('@cloudflare/workers-types').D1Database;
+
+declare namespace App {
+    interface Locals {
+        runtime: {
+            env: {
+                DB: D1Database;
+            };
+        };
+    }
+}
