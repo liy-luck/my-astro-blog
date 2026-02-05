63eb8798222eb3a948a18ca54ba8a6852890e249
liy <761686949@qq.com>
2026-02-05 23:15:52 +0800

    优化

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server', // 必须是 server，表示开启服务端渲染 (SSR)
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [tailwind()]
 });
